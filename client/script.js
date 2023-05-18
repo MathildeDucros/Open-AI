2a801f33def7fe468961ed0bf302f1a33db36439
Mathilde <ducros.mathilde@gmail.com>
2023-05-18 15:26:04 +0200

    last commit

--- client/script.js
+++ client/script.js
@@ -93,7 +93,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   //fetch data from server -> bot's response
-  const response = await fetch("http://localhost:5050", {
+  const response = await fetch("https://codex-open-ai-9lfe.onrender.com", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
